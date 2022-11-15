569d173f57954ed2271d7c56a5175bdeee144f58
Arjun <dotadex1980@gmail.com>
2022-11-15 21:31:15 +0530

    added project bill

---
+++ public/add-project-bill-form.php
@@ -0,0 +1,213 @@
+<?php
+include_once('includes/functions.php');
+$function = new functions;
+include_once('includes/custom-functions.php');
+$fn = new custom_functions;
+
+?>
+<?php
+if (isset($_POST['btnAdd'])) {
+
+    $project_id = $db->escapeString(($_POST['project_id']));
+    $amount = $db->escapeString($_POST['amount']);
+    $balance = $db->escapeString($_POST['balance']);
+    $date = $db->escapeString($_POST['date']);
+
+    // get image info
+    $menu_image = $db->escapeString($_FILES['product_image']['name']);
+    $image_error = $db->escapeString($_FILES['product_image']['error']);
+    $image_type = $db->escapeString($_FILES['product_image']['type']);
+
+    // create array variable to handle error
+    $error = array();
+    // common image file extensions
+    $allowedExts = array("gif", "jpeg", "jpg", "png");
+
+    // get image file extension
+    error_reporting(E_ERROR | E_PARSE);
+    $extension = end(explode(".", $_FILES["product_image"]["name"]));
+
+    if (empty($project_id)) {
+        $error['project_id'] = " <span class='label label-danger'>Required!</span>";
+    }
+    if (empty($amount)) {
+        $error['amount'] = " <span class='label label-danger'>Required!</span>";
+    }
+    if (empty($balance)) {
+        $error['balance'] = " <span class='label label-danger'>Required!</span>";
+    }
+    if (empty($date)) {
+        $error['date'] = " <span class='label label-danger'>Required!</span>";
+    }
+
+
+    if (!empty($project_id) && !empty($amount) && !empty($balance) && !empty($date)) {
+        $result = $fn->validate_image($_FILES["product_image"]);
+        // create random image file name
+        $string = '0123456789';
+        $file = preg_replace("/\s+/", "_", $_FILES['product_image']['name']);
+        $menu_image = $function->get_random_string($string, 4) . "-" . date("Y-m-d") . "." . $extension;
+
+        // upload new image
+        $upload = move_uploaded_file($_FILES['product_image']['tmp_name'], 'upload/staffs/' . $menu_image);
+
+        // insert new data to menu table
+        $upload_image = 'upload/staffs/' . $menu_image;
+
+
+
+        $sql_query = "INSERT INTO project_bill (project_id,amount,balance,image,date)VALUES('$project_id','$amount','$balance','$upload_image','$date')";
+        $db->sql($sql_query);
+        $result = $db->getResult();
+        if (!empty($result)) {
+            $result = 0;
+        } else {
+            $result = 1;
+        }
+
+        if ($result == 1) {
+
+            $error['add_staff'] = "<section class='content-header'>
+                                                <span class='label label-success'>Staff Added Successfully</span> </section>";
+        } else {
+            $error['add_staff'] = " <span class='label label-danger'>Failed</span>";
+        }
+    }
+}
+?>
+<section class="content-header">
+    <h1>Add Project Bill <small><a href='project-bill.php'> <i class='fa fa-angle-double-left'></i>&nbsp;&nbsp;&nbsp;Back to Project Bill</a></small></h1>
+
+    <?php echo isset($error['add_staff']) ? $error['add_staff'] : ''; ?>
+    <ol class="breadcrumb">
+        <li><a href="home.php"><i class="fa fa-home"></i> Home</a></li>
+    </ol>
+    <hr />
+</section>
+<section class="content">
+    <div class="row">
+        <div class="col-md-10">
+
+            <!-- general form elements -->
+            <div class="box box-primary">
+                <div class="box-header with-border">
+
+                </div>
+                <!-- /.box-header -->
+                <!-- form start -->
+                <form name="add_staff_form" method="post" enctype="multipart/form-data">
+                    <div class="box-body">
+                        <div class="row">
+                            <div class="form-group">
+                                <div class="col-md-6">
+                                    <label for="exampleInputEmail1"> Project ID</label> <i class="text-danger asterik">*</i><?php echo isset($error['project_id']) ? $error['project_id'] : ''; ?>
+                                    <select id='project_id' name="project_id" class='form-control' required>
+                                        <option value="">Select</option>
+                                        <?php
+                                        $sql = "SELECT id,name FROM `projects`";
+                                        $db->sql($sql);
+                                        $result = $db->getResult();
+                                        foreach ($result as $value) {
+                                        ?>
+                                            <option value='<?= $value['id'] ?>'><?= $value['name'] ?></option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <label for="exampleInputEmail1">Amount</label> <i class="text-danger asterik">*</i><?php echo isset($error['amount']) ? $error['amount'] : ''; ?>
+                                    <input type="text" class="form-control" name="amount" required />
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="form-group">
+                                <div class="col-md-6">
+                                    <label for="exampleInputEmail1">Balance</label> <i class="text-danger asterik">*</i><?php echo isset($error['balance']) ? $error['balance'] : ''; ?>
+                                    <input type="text" class="form-control" name="balance" required>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <label for="exampleInputFile">Receipt</label> <i class="text-danger asterik">*</i><?php echo isset($error['product_image']) ? $error['product_image'] : ''; ?>
+                                    <input type="file" name="product_image" onchange="readURL(this);" accept="image/png,  image/jpeg" id="product_image" required />
+                                    <img id="blah" src="#" alt="" />
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <div class="form-group">
+                                <div class="col-md-6">
+                                    <label for="exampleInputEmail1">Date</label> <i class="text-danger asterik">*</i><?php echo isset($error['date']) ? $error['date'] : ''; ?>
+                                    <input type="date" class="form-control" name="date" required>
+                                </div>
+
+
+
+                            </div>
+                            <br>
+
+
+
+                        </div>
+
+                        <!-- /.box-body -->
+
+                        <div class="box-footer">
+                            <button type="submit" class="btn btn-primary" name="btnAdd">Add</button>
+                            <input type="reset" onClick="refreshPage()" class="btn-warning btn" value="Clear" />
+                        </div>
+
+                </form>
+
+            </div><!-- /.box -->
+        </div>
+    </div>
+</section>
+
+<div class="separator"> </div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+<script>
+    $('#add_staff_form').validate({
+
+        ignore: [],
+        debug: false,
+        rules: {
+            name: "required",
+            role: "required",
+            category_image: "required",
+            mobile: "required",
+        }
+    });
+    $('#btnClear').on('click', function() {
+        for (instance in CKEDITOR.instances) {
+            CKEDITOR.instances[instance].setData('');
+        }
+    });
+</script>
+<script>
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                $('#blah')
+                    .attr('src', e.target.result)
+                    .width(150)
+                    .height(200);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
+
+<!--code for page clear-->
+<script>
+    function refreshPage() {
+        window.location.reload();
+    }
+</script>
+
+<?php $db->disconnect(); ?>
---
+++ public/edit-project-bill-form.php
@@ -0,0 +1,187 @@
+<?php
+include_once('includes/functions.php');
+$function = new functions;
+include_once('includes/custom-functions.php');
+$fn = new custom_functions;
+?>
+<?php
+
+if (isset($_GET['id'])) {
+    $ID = $db->escapeString($_GET['id']);
+} else {
+    // $ID = "";
+    return false;
+    exit(0);
+}
+
+if (isset($_POST['btnEdit'])) {
+
+    $project_id = $db->escapeString(($_POST['project_id']));
+    $amount = $db->escapeString($_POST['amount']);
+    $balance = $db->escapeString($_POST['balance']);
+    $date = $db->escapeString($_POST['date']);
+    $error = array();
+
+
+    if (empty($project_id)) {
+        $error['project_id'] = " <span class='label label-danger'>Required!</span>";
+    }
+    if (empty($amount)) {
+        $error['amount'] = " <span class='label label-danger'>Required!</span>";
+    }
+    if (empty($balance)) {
+        $error['balance'] = " <span class='label label-danger'>Required!</span>";
+    }
+    if (empty($date)) {
+        $error['date'] = " <span class='label label-danger'>Required!</span>";
+    }
+
+
+
+
+
+    if (!empty($project_id) && !empty($amount) && !empty($balance) && !empty($date)) {
+        if ($_FILES['image']['size'] != 0 && $_FILES['image']['error'] == 0 && !empty($_FILES['image'])) {
+            //image isn't empty and update the image
+            $old_image = $db->escapeString($_POST['old_image']);
+            $extension = pathinfo($_FILES["image"]["name"])['extension'];
+
+            $result = $fn->validate_image($_FILES["image"]);
+            $target_path = 'upload/staffs/';
+
+            $filename = microtime(true) . '.' . strtolower($extension);
+            $full_path = $target_path . "" . $filename;
+            if (!move_uploaded_file($_FILES["image"]["tmp_name"], $full_path)) {
+                echo '<p class="alert alert-danger">Can not upload image.</p>';
+                return false;
+                exit();
+            }
+            if (!empty($old_image)) {
+                unlink($old_image);
+            }
+            $upload_image = 'upload/staffs/' . $filename;
+            $sql = "UPDATE project_bill SET `image`='" . $upload_image . "' WHERE `id`=" . $ID;
+            $db->sql($sql);
+        }
+
+        $sql_query = "UPDATE project_bill SET project_id='$project_id',amount='$amount',balance='$balance',date='$date' WHERE id =  $ID";
+        $db->sql($sql_query);
+        $res = $db->getResult();
+        $update_result = $db->getResult();
+        if (!empty($update_result)) {
+            $update_result = 0;
+        } else {
+            $update_result = 1;
+        }
+
+        // check update result
+        if ($update_result == 1) {
+
+            $error['update_staff'] = " <section class='content-header'><span class='label label-success'>Staff Details updated Successfully</span></section>";
+        } else {
+            $error['update_staff'] = " <span class='label label-danger'>Failed to update</span>";
+        }
+    }
+}
+
+
+// create array variable to store previous data
+$data = array();
+
+$sql_query = "SELECT * FROM project_bill WHERE id =" . $ID;
+$db->sql($sql_query);
+$res = $db->getResult();
+
+if (isset($_POST['btnCancel'])) { ?>
+    <script>
+        window.location.href = "project_bill.php";
+    </script>
+<?php } ?>
+<section class="content-header">
+    <h1>
+        Edit Project Bill<small><a href='project_bill.php'><i class='fa fa-angle-double-left'></i>&nbsp;&nbsp;&nbsp;Back to Project Bill</a></small></h1>
+    <small><?php echo isset($error['update_staff']) ? $error['update_staff'] : ''; ?></small>
+    <ol class="breadcrumb">
+        <li><a href="home.php"><i class="fa fa-home"></i> Home</a></li>
+    </ol>
+</section>
+<section class="content">
+    <!-- Main row -->
+
+    <div class="row">
+        <div class="col-md-10">
+
+            <!-- general form elements -->
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Edit Project Bill</h3>
+                </div><!-- /.box-header -->
+                <!-- form start -->
+                <form id="edit_staff_form" method="post" enctype="multipart/form-data">
+                    <div class="box-body">
+                        <input type="hidden" id="old_image" name="old_image" value="<?= $res[0]['image']; ?>">
+                        <div class="row">
+                            <div class="form-group">
+                                <div class='col-md-6'>
+                                    <label for="exampleInputEmail1">Project ID</label> <i class="text-danger asterik">*</i>
+                                    <select id='project_id' name="project_id" class='form-control' required>
+                                        <option value="none">Select</option>
+                                        <?php
+                                        $sql = "SELECT id,name FROM `project_bill`";
+                                        $db->sql($sql);
+
+                                        $result = $db->getResult();
+                                        foreach ($result as $value) {
+                                        ?>
+                                            <option value='<?= $value['id'] ?>' <?= $value['id'] == $res[0]['name'] ? 'selected="selected"' : ''; ?>><?= $value['name'] ?></option>
+
+                                        <?php } ?>
+                                    </select>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <label for="exampleInputEmail1">Amount</label> <i class="text-danger asterik">*</i><?php echo isset($error['amount']) ? $error['amount'] : ''; ?>
+                                    <input type="text" class="form-control" name="amount" value="<?php echo $res[0]['amount']; ?>" />
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="form-group">
+                                <div class="col-md-6">
+                                    <label for="exampleInputEmail1">Balance</label> <i class="text-danger asterik">*</i><?php echo isset($error['balance']) ? $error['balance'] : ''; ?>
+                                    <input type="number" class="form-control" name="balance" value="<?php echo $res[0]['balance']; ?>">
+                                </div>
+
+                                <div class="col-md-4">
+                                    <label for="exampleInputFile">Receipt</label>
+
+                                    <input type="file" accept="image/png,  image/jpeg" onchange="readURL(this);" name="image" id="image">
+                                    <p class="help-block"><img id="blah" src="<?php echo $res[0]['image']; ?>" style="height:120px;max-width:100%" /></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group">
+                                <div class="col-md-6">
+                                    <label for="exampleInputEmail1">Date</label> <i class="text-danger asterik">*</i><?php echo isset($error['date']) ? $error['date'] : ''; ?>
+                                    <input type="date" class="form-control" name="date" value="<?php echo $res[0]['date']; ?>">
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div><!-- /.box-body -->
+
+                    <div class="box-footer">
+                        <button type="submit" class="btn btn-primary" name="btnEdit">Update</button>
+
+                    </div>
+                </form>
+            </div><!-- /.box -->
+        </div>
+    </div>
+</section>
+
+<div class="separator"> </div>
+<?php $db->disconnect(); ?>
